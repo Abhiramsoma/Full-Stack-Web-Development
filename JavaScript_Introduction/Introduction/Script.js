5c32a1277c82f53ca73d5d83da9964299f00af04
Abhiramsoma <114901769+Abhiramsoma@users.noreply.github.com>
2025-11-28 21:16:32 +0530

    Add files via upload

---
+++ JavaScript_Introduction/Introduction/Script.js
@@ -0,0 +1,8 @@
+
+// console.log("Hello World")
+//console.error("This is an error message")
+// console.warn("This is a warning message")
+
+//alert("This is an alert")
+//prompt("Enter the Number")
+//confirm("Are you sure?")
