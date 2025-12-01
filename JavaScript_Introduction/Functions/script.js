316c46193ac09e8e0e908f6302a250e470c66b03
Abhiramsoma <114901769+Abhiramsoma@users.noreply.github.com>
2025-12-01 20:08:03 +0530

    Add files via upload

---
+++ JavaScript_Introduction/Functions/script.js
@@ -0,0 +1,20 @@
+//functions
+function greet(){
+    console.log("Hello, World!");
+}
+greet();
+//function with parameter
+function namaste(name){//parameter=name
+    console.log(name);
+}
+namaste("Hello duniya")
+//argument=Hello duniya
+
+var abcd = function(){
+    console.log("I am a  function");
+}
+
+
+function ab(){
+    return 12; 
+}
